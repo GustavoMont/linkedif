dfadb62eea7c52d730f65b73c3eb95fcd2cc1b71
GustavoMont <lgmluisgm@gmail.com>
2021-09-13 09:54:20 -0300

    :tada: validate form

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,5 +1,62 @@
 const form = document.forms[0]
 
-form.onsubmit = (e) =>{
+function isInvalidoInfo(inputsGroup){
+    let invalido = false
+    inputsGroup.forEach(input => {
+        if (input.value === '' || input.value.length <= 2) {
+            invalido = true
+            input.value = ''
+            input.style.borderColor = '#f91000'
+            input.placeholder = `CAMPO OBRIGATÓRIO`
+        }
+        else{
+            input.style.borderColor = 'rgb(172, 172, 172)'
+        }
+    })
+    return invalido;
+}
+
+
+function isSalarioInvalido(salario){
+    if (isNaN(salario)) {
+        form.salario.value = ''
+        form.salario.style.borderColor = '#f91000'
+        form.salario.placeholder = 'Somente números'
+        return true
+    }
+    if (!isNaN(salario) && salario <= 500) {
+        form.salario.value = ''
+        form.salario.style.borderColor = '#f91000'
+        form.salario.placeholder = 'Salário muito baixo'
+        return true
+    }
+    return false
+}
+
+form.onsubmit = (e) => {
     e.preventDefault()
+    const obrigatorios = document.querySelectorAll('[required]')
+    
+    const cargo = form.cargo.value
+    const empresa = form.empresa.value
+    const salario = parseFloat(form.salario.value)    
+    const local = form.local.value === '' ? 'Remoto' : form.local.value
+    const contato = form.contato.value
+    const descricao = form.descricao.value
+    const dev = form.dev.checked
+    
+    if (isInvalidoInfo(obrigatorios) || isSalarioInvalido(salario)) {
+        return
+    }
+
+    const vagasExistentes = JSON.parse(localStorage.getItem('@vagas'));
+    const vagasAtualizadas = [{cargo, empresa, salario, local, contato, descricao, dev}, ...vagasExistentes]
+
+    localStorage.setItem('@vagas', JSON.stringify(vagasAtualizadas))
+    Object.values(form).forEach(input =>{
+        input.value = ''
+    })
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = `<p>Vaga Cadastrada com Sucesso</p>`
+
 }
--- scripts/vagas.js
+++ scripts/vagas.js
@@ -1,9 +1,5 @@
-const vagasCadastradas = [ {cargo: 'Gerente de Estoque', empresa: 'Bretas', salario: 2900, local: 'Pirapor/MG', descricao: 'Coordenar, recepcionar, conferir e supervisionar o armazenamento dos produtos e materiais no estoque. Realizar os lançamentos da movimentação de entradas, saídas e controla os estoques. Controlar e gerenciar o inventário diário. Conferir a chegada e saída do seu turno.', dev: false, contato: 'https://www.bretas.com.br/trabalhe-conosco/'}, {cargo: 'Desenvolvedor Front-end', empresa: 'WebTaxi', salario: 6900, local: 'Remoto', descricao: 'Desenvolver a parte visual do nosso site, fazendo com que tenha uma ótima usabilidade e responsividade. Precisa entender de HTML, CSS, javascript, alguns frameworks como jQuery e Bootstrap e ter algumas noções de design. Saber usar programas edição de imagem e ilustração será um diferencial. Oferecemos plano de saúde e cartão de alimentação,', dev: true, contato: '#'}, {cargo: 'Operador de Telemarketing', empresa: 'AeC', salario: 1100, local: 'Remoto', descricao: 'Os nossos atendentes são mais que fundamentais na AeC, são o nosso coração! Você estará intermediando a comunicação entre cliente e usuário final por meio dos nossos canais atendimento. A atuação pode ocorrer em via voz, chat ou BackOffice. Pela grande quantidade de clientes e serviços que prestamos, temos vagas destinadas para primeiro emprego ou até para graduados. Pra quem gosta de suporte, atendimento receptivo ou ativo. Para trabalhar home office ou no escritório. Sempre 6 horas por dia e vagas em diversos turnos... É uma infinidade de opções!', dev: false, contato: 'https://queroser.aec.com.br/'}]
 
 document.body.onload = () => {
-    if (!localStorage.getItem('@vagas')){
-        localStorage.setItem('@vagas', JSON.stringify(vagasCadastradas))
-    }
     const vagas =  JSON.parse(localStorage.getItem('@vagas'))
     const vagasSection = document.querySelector('#vagas')
 
@@ -25,7 +21,7 @@ document.body.onload = () => {
                 </span>
             </p>
             </div>
-        <a href=${item.contato} class="button">CADIDATAR-SE</a>
+        <a href=${item.contato} target="blank" class="button">CADIDATAR-SE</a>
     </div> `
         vagasSection.innerHTML += template
     
